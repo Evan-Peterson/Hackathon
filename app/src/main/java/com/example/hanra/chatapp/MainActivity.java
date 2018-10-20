864dbc9954b9e732f3f8570ac4ea5ff0eef7d6c8
evanp <evanp@iastate.edu>
2018-10-20 03:45:26 -0500

    added cards

--- app/src/main/java/com/example/hanra/chatapp/MainActivity.java
+++ app/src/main/java/com/example/hanra/chatapp/MainActivity.java
@@ -11,21 +11,27 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 
+import static com.example.hanra.chatapp.R.string.*;
+
 public class MainActivity extends AppCompatActivity {
     //hello evan i want to seee if this works
 
-    private String m_Text = "";
+    private static String m_Text = "test";
     private static final String TAG = "MainActivity";
     private static final int ERROR_DIALOG_REQUEST = 9001;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView helloTextView = (TextView) findViewById(R.id.info_text);
+        helloTextView.setText(m_Text);
 
         final Button addCommentButton = (Button) findViewById(R.id.addCommentButton);
         addCommentButton.setOnClickListener(new View.OnClickListener() {
@@ -49,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         m_Text = input.getText().toString();
+                        helloTextView.setText(m_Text);
                     }
                 });
                 builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -61,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
                 builder.show();
             }
         });
+        helloTextView.setText(m_Text);
         if (isServicesOK()) {
             init();
         }
@@ -88,4 +96,9 @@ public class MainActivity extends AppCompatActivity {
         }
         return false;
     }
+    /*
+    public static String getString() {
+        return m_Text;
+    }
+    */
 }
