1851f6e07785b30d03b41553f1b13752dd746ddb
evanp <evanp@iastate.edu>
2018-10-20 00:44:27 -0500

    added dialouge box for button press

--- app/src/main/java/com/example/hanra/chatapp/MainActivity.java
+++ app/src/main/java/com/example/hanra/chatapp/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.hanra.chatapp;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     //hello evan i want to seee if this works
+
+    private String m_Text = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,8 +22,36 @@ public class MainActivity extends AppCompatActivity {
 
         final Button addCommentButton = (Button) findViewById(R.id.addCommentButton);
         addCommentButton.setOnClickListener(new View.OnClickListener() {
+
+
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Hello world", Toast.LENGTH_SHORT).show();
+
+                Context context = MainActivity.this;
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("Title");
+
+                // Set up the input
+
+                final EditText input = new EditText(context);
+                // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
+                builder.setView(input);
+
+// Set up the buttons
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        m_Text = input.getText().toString();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                builder.show();
             }
         });
     }
