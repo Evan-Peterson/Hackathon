ffd3c45b4402a86d6e6256dc93296ed9ba0aa028
Aarodynamics3 <hanrahanaaron@gmail.com>
2018-10-20 01:00:20 -0500

    added google play version check

--- app/src/main/java/com/example/hanra/chatapp/MainActivity.java
+++ app/src/main/java/com/example/hanra/chatapp/MainActivity.java
@@ -1,20 +1,27 @@
 package com.example.hanra.chatapp;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+
 public class MainActivity extends AppCompatActivity {
     //hello evan i want to seee if this works
 
     private String m_Text = "";
+    private static final String TAG = "MainActivity";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
                 input.setInputType(InputType.TYPE_CLASS_TEXT);
                 builder.setView(input);
 
-// Set up the buttons
+                // Set up the buttons
                 builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -54,5 +61,31 @@ public class MainActivity extends AppCompatActivity {
                 builder.show();
             }
         });
+        if (isServicesOK()) {
+            init();
+        }
+    }
+
+    private void init() {
+
+    }
+
+    public boolean isServicesOK(){
+        Log.d(TAG, "isServicesOK: checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+        if (available == ConnectionResult.SUCCESS) {
+            // everything is fine and user can make map requests
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            // error occured but resolvable (version err.)
+            Log.d(TAG, "isServicesOK: an error occurred but can be fixed");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "We can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }
